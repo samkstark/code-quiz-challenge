0c9096b21a5360475b3e1af73f9416c9911ec242
Samantha <samantha@dbas-Mac-mini.local>
2022-11-13 15:17:34 -0800

    adding first bits of js

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const scoreText = document.querySelector('#score');
+
+
+
